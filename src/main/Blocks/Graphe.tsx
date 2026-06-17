fb1bf477e416942cbe5958d74d454c8c55fb7ca1
Shaya <3x0de.dev@gmail.com>
2026-06-17 21:39:35 +0200

    ⋆༺𓆩☠︎︎ Utilisation d'un graphe perso ☠︎︎𓆪༻⋆

--- src/main/Blocks/Graphe.tsx
+++ src/main/Blocks/Graphe.tsx
@@ -1,29 +1,47 @@
-function Graphe() {
+import { useState, useCallback } from "react";
+import {
+  ReactFlow,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+
+const initialNodes = [
+  { id: "n1", position: { x: 0, y: 0 }, data: { label: "Node 1" } },
+  { id: "n2", position: { x: 100, y: 100 }, data: { label: "Node 2" } },
+];
+const initialEdges = [{ id: "n1-n2", source: "n1", target: "n2" }];
+
+export default function Test() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes) =>
+      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes) =>
+      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    [],
+  );
+  const onConnect = useCallback(
+    (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    [],
+  );
+
   return (
-    <h1>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni ipsum non
-      consectetur saepe adipisci quidem eius illum sed nisi autem culpa,
-      officiis similique asperiores unde harum deleniti itaque maxime reiciendis
-      architecto beatae animi nulla error? Tempore quis harum quidem nostrum
-      esse quas eveniet, nulla molestiae commodi hic atque quasi voluptates
-      cumque, iste ducimus libero provident? Quasi architecto unde ipsum
-      accusamus a incidunt deleniti, odio quis aspernatur libero corporis
-      consequuntur harum? Expedita repellendus fugiat necessitatibus vero illum
-      fuga qui quisquam porro, sed in. Labore, sit doloremque repudiandae culpa
-      aliquid voluptates, placeat iure fugit laboriosam ea ducimus accusamus ut
-      qui itaque. Quam dolor deleniti nam corporis exercitationem atque,
-      blanditiis cum, minima quasi odio ea vel quidem eos voluptatem suscipit!
-      Laborum tempore nemo dolorum ut possimus praesentium eos. Odit, eius
-      alias? Accusamus eligendi labore cupiditate sit reprehenderit harum, enim
-      vel suscipit quia! Porro ipsum necessitatibus vero fugit hic nobis. Ea
-      similique eveniet repudiandae consequuntur, delectus tempore facere fugit
-      exercitationem explicabo a dolorem quaerat quisquam, ex eaque velit,
-      nesciunt vitae! Saepe non at consectetur repellendus quasi eos praesentium
-      est minima fuga laboriosam deserunt optio consequuntur, alias, nesciunt
-      quisquam sunt? Nam animi, quidem ex sint eligendi itaque quibusdam quas
-      sequi reiciendis? Veritatis totam optio ea!
-    </h1>
+    <div style={{ width: "100%", aspectRatio: 1 }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      />
+    </div>
   );
 }
-
-export default Graphe;
