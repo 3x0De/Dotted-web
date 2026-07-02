f7438da9787138ab88c3a624ca1664ac2ab37734
Shaya <3x0de.dev@gmail.com>
2026-07-02 15:41:03 +0200

    ⋆༺𓆩☠︎︎ début de la page ☠︎︎𓆪༻⋆ - catégories - personaliastion des icons

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,37 +1,68 @@
 import { STATE } from "./types/menu";
 import Wrapper from "./Wrapper";
+import "./styles/Page.scss";
+import baniere from "../public/Icons/logo_max.svg";
 
 function Page() {
   return (
-    <Wrapper
-      init={{
-        id: 0,
-        type: STATE.col,
-        content: [
-          { id: Math.random(), type: STATE.h1, content: "1" },
-          { id: Math.random(), type: STATE.h1, content: "2" },
+    <div id="Page">
+      <Header />
+      <Wrapper
+        init={{
+          id: 0,
+          type: STATE.col,
+          content: [
+            { id: Math.random(), type: STATE.h1, content: "1" },
+            { id: Math.random(), type: STATE.h1, content: "2" },
+            {
+              id: Math.random(),
+              type: STATE.row,
+              content: [
+                {
+                  id: Math.random(),
+                  type: STATE.col,
+                  content: [
+                    { id: Math.random(), type: STATE.h1, content: "1" },
+                    { id: Math.random(), type: STATE.h1, content: "2" },
+                  ],
+                },
+                {
+                  id: Math.random(),
+                  type: STATE.col,
+                  content: [
+                    { id: Math.random(), type: STATE.h1, content: "1" },
+                  ],
+                },
+              ],
+            },
+          ],
+        }}
+      />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <div
+        id="Banniere"
+        style={
           {
-            id: Math.random(),
-            type: STATE.row,
-            content: [
-              {
-                id: Math.random(),
-                type: STATE.col,
-                content: [
-                  { id: Math.random(), type: STATE.h1, content: "1" },
-                  { id: Math.random(), type: STATE.h1, content: "2" },
-                ],
-              },
-              {
-                id: Math.random(),
-                type: STATE.col,
-                content: [{ id: Math.random(), type: STATE.h1, content: "1" }],
-              },
-            ],
-          },
-        ],
-      }}
-    />
+            "--Baniere-Image": `url('/Icons/logo_max.svg')`,
+          } as React.CSSProperties
+        }
+      ></div>
+      <div
+        id="Icon"
+        style={
+          {
+            "--Icon-Image": `url('/Icons/logo_mini.svg')`,
+          } as React.CSSProperties
+        }
+      ></div>
+      <input id="Titre" type="text" placeholder="Nouvelle page..." />
+    </header>
   );
 }
 
