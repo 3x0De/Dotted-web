4233cf5ab3cce4a90909cd50092d556a006d01dd
Shaya <3x0de.dev@gmail.com>
2026-07-09 22:46:03 +0200

    ⋆༺𓆩☠︎︎ Routes ☠︎︎𓆪༻⋆

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Principal from "./main/Principal";
 
 import "./styles/App.scss";
 
 function App() {
-  return <Principal />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Principal />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
