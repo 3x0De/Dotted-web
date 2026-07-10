79744bbb4d3350df0257bc270dd046f9244458db
Shaya <3x0de.dev@gmail.com>
2026-07-10 02:27:18 +0200

    ⋆༺𓆩☠︎︎ Erreurs ☠︎︎𓆪༻⋆ + 500

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Error401 from "./error/401";
 import Error403 from "./error/403";
 import Error404 from "./error/404";
 import Error418 from "./error/418";
+import Error500 from "./error/500";
 
 import "./styles/App.scss";
 
@@ -20,6 +21,7 @@ function App() {
         <Route path="/403" element={<Error403 />} />
         <Route path="/404" element={<Error404 />} />
         <Route path="/418" element={<Error418 />} />
+        <Route path="/500" element={<Error500 />} />
         <Route path="*" element={<Principal />} />
       </Routes>
     </BrowserRouter>
---
+++ src/error/500.tsx
@@ -0,0 +1,7 @@
+import Error from "./Error";
+
+function Error500() {
+  return <Error msg="Erreur 500 - Internal Server Error" />;
+}
+
+export default Error500;
